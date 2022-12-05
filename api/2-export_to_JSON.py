906d27430e934548e899d9e0509d5f51905eb512
DaisyGeraldine <chipanal@hotmail.com>
2022-12-05 09:07:32 -0500

    fix: probate to checker

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Module script to export data in the JSON format """
+""" Module in script to export data in the JSON format """
 from requests import get
 from sys import argv
 import json
